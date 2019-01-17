58f7f2d4690eef4387e79938352d732d343a884d
Nicolas Boisteault <nboisteault@3liz.com>
2019-01-17 11:11:06 +0100

    Ajout du script d'upgrade

---
+++ mapBuilder/install/upgrade.php
@@ -0,0 +1,17 @@
+<?php
+class mapBuilderModuleUpgrader extends jInstallerModule {
+
+    public $targetVersions = array(
+        'v1.0.0-rc.8'
+    );
+    public $date = '2019-01-17';
+
+    function install() {
+        // Add mapcontext table
+        if( $this->firstDbExec() ) {
+            $this->useDbProfile('jauth');
+            $this->execSQLScript('sql/mapcontext');
+        }
+    }
+
+}
