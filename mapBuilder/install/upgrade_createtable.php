9f07e732a407d75b31eefc8ae18f7bd6eb7dd360
Nicolas Boisteault <nboisteault@3liz.com>
2019-01-18 16:32:26 +0100

    v1.0.0-rc.9

--- mapBuilder/install/upgrade_createtable.php
+++ mapBuilder/install/upgrade_createtable.php
@@ -2,7 +2,7 @@
 class mapBuilderModuleUpgrader_createtable extends jInstallerModule {
 
     public $targetVersions = array(
-        'v1.0.0-rc.10'
+        'v1.0.0-rc.9'
     );
     public $date = '2019-01-18';
 
