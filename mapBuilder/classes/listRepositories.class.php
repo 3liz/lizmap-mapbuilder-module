26e8ce5db529c8eaa3e92a26f899db5be3600546
Nicolas Boisteault <nboisteault@3liz.com>
2019-03-29 12:51:31 +0100

    Fix missing class. V1.0.1

---
+++ mapBuilder/classes/listRepositories.class.php
@@ -0,0 +1,41 @@
+<?php 
+
+require_once (JELIX_LIB_PATH.'forms/jFormsDatasource.class.php');
+
+class listRepositories implements jIFormsDatasource
+{
+  protected $formId = 0;
+
+  protected $data = array();
+
+  function __construct($id)
+  {
+    $this->formId = $id;
+
+    $repositories = array();
+
+    foreach (lizmap::getRepositoryList() as $repositoryName) {
+        $repository = lizmap::getRepository($repositoryName);
+        if( jAcl2::check('lizmap.repositories.view', $repository->getKey() )){
+          $repositories[$repository->getKey()] = $repository->getData('label');
+        }
+    }
+
+    $this->data = $repositories;
+  }
+
+  public function getData($form)
+  {
+    return ($this->data);
+  }
+
+  public function getLabel($key)
+  {
+    if(isset($this->data[$key]))
+      return $this->data[$key];
+    else
+      return null;
+  }
+
+}
+?>
