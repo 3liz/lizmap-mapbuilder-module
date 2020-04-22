2a7ee15bc335b386ef72f74babe36e3a2b6712bc
Laurent Jouanneau <dev@ljouanneau.com>
2020-04-22 18:00:45 +0200

    Fix installer of mapBuilderAdmin
    It modified the wrong ini file. It should modify only
    localconfig.ini.php

--- mapBuilderAdmin/install/install.php
+++ mapBuilderAdmin/install/install.php
@@ -12,26 +12,15 @@
 class mapBuilderAdminModuleInstaller extends jInstallerModule {
 
     function install() {
-        //if ($this->firstDbExec())
-        //    $this->execSQLScript('sql/install');
-
-        /*if ($this->firstExec('acl2')) {
-            jAcl2DbManager::addSubject('my.subject', 'mapBuilderAdmin~acl.my.subject', 'subject.group.id');
-            jAcl2DbManager::addRight('admins', 'my.subject'); // for admin group
-        }
-        */
         if ($this->entryPoint->getEpId() == 'admin') {
-            // check first if localconfig has already redefined the list of controllers
-            $adminControllers = $this->entryPoint->localConfigIni->getValue('admin', 'simple_urlengine_entrypoints');
-            if (!$adminControllers) {
-                // not in localconfig, retrieve list from mainconfig
-                $adminControllers = $this->entryPoint->configIni->getValue('admin', 'simple_urlengine_entrypoints');
-            }
+            $localConfigIni = $this->entryPoint->localConfigIni->getMaster();
+
+            $adminControllers = $localConfigIni->getValue('admin', 'simple_urlengine_entrypoints');
             $mbCtrl = 'mapBuilderAdmin~*@classic';
             if (strpos($adminControllers, $mbCtrl) === false) {
                 // let's register mapBuilderAdmin controllers
                 $adminControllers .= ', '.$mbCtrl;
-                $this->entryPoint->localConfigIni->setValue('admin', $adminControllers, 'simple_urlengine_entrypoints');
+                $localConfigIni->setValue('admin', $adminControllers, 'simple_urlengine_entrypoints');
             }
         }
     }
