3d58b3bd008376cb98687f9a389372a1f94bab70
neo <neo.konikoff-garaix01@etu.umontpellier.fr>
2024-04-09 12:33:14 +0200

    FIX : Confguration of Map init

--- mapBuilder/install/conf/mapBuilder.ini.php
+++ mapBuilder/install/conf/mapBuilder.ini.php
@@ -5,7 +5,7 @@
 repository=
 
 ;; put here original extent (WGS 84) when mapBuilder load. Default is metropolitan France.
-extent="-4.65,51.68,9.10,40.63"
+extent="-4.65,40.63,9.10,51.68"
 
 ;; set base layer (osmMapnik, osmStamenToner, osmCyclemap, bingStreets, bingSatellite, bingHybrid, ignTerrain, ignStreets, ignSatellite, ignCadastral)
 baseLayer=osmMapnik
