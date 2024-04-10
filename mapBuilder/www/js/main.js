e10376279c6b559a79f8f07e764f2b00e0e9a15e
neo <neo.konikoff-garaix01@etu.umontpellier.fr>
2024-04-10 11:09:33 +0200

    BOOTSTRAP : retour v4.3.1, problème compatibilité et peu d'utilité de la 5 pour l'utilisation actuelle

--- mapBuilder/www/js/main.js
+++ mapBuilder/www/js/main.js
@@ -26,8 +26,6 @@ import {always as alwaysCondition, shiftKeyOnly as shiftKeyOnlyCondition} from '
 
 import './modules/bottom-dock.js';
 
-import 'bootstrap';
-
 // Extent on metropolitan France if not defined in mapBuilder.ini.php
 var originalCenter = [217806.92414447578, 5853470.637803803];
 var originalZoom = 6;
